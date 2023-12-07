682094becfb8476f1a039f1b8b04ea91d702ce18
dykmalon <dykmalon@gmail.com>
2023-12-07 14:42:28 +0530

    newcoin

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -4,12 +4,12 @@ const Coin = () => {
 
     return (
         <div className="coin-div">
-            <div className="coin">
+            {/* <div className="coin">
                 <div className="front"></div>
                 <div className="front_b"></div>
                 <div className="back"></div>
                 <div className="back_b"></div>
-            </div>
+            </div> */}
         </div>
     );
 };
