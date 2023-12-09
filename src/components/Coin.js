ebb961aefcf116fa7d338c796ecfcdcd3a90e09a
dykmalon <dykmalon@gmail.com>
2023-12-09 06:59:24 +0530

    newconfetti

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,11 +1,38 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import confetti from 'canvas-confetti';
 
 const Coin = () => {
+    useEffect(() => {
+        const likeButton = document.getElementById("like");
 
-    
+        likeButton.addEventListener("click", () => {
+            likeOrLove(likeButton);
+        });
+
+        function likeOrLove(button) {
+            if (!button.classList.contains("clicked")) {
+                function randomInRange(min, max) {
+                    return Math.random() * (max - min) + min;
+                }
+
+                setTimeout(() => {
+                    confetti({
+                        particleCount: 300,
+                        spread: 100,
+                        origin: { y: 1 }
+                    });
+                }, 400);
+                button.classList.add("clicked");
+                setTimeout(() => {
+                    button.classList.remove("clicked");
+                }, 800);
+            }
+        }
+    }, []);
 
     return (
-        <div id="button3" className="coin-div pulsate">
+        <div className="coin-div round-button" type="button" id="like">
+          
         </div>
     );
 };
